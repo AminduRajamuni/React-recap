5bd5471c51cd402230462acc3ff09a1e1e4680d5
UchihaKiller1 <amindurajamuni@gmail.com>
2025-11-30 14:32:14 +0530

    hello

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import mongoose from "mongoose"
+
+
+const app = express()
+const PORT = 5000
+
+app.listen(PORT,()=>{
+
+console.log(`Server is running on port ${PORT}`);
+})
+
+
